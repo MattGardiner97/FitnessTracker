1c90e5e83d184910fdba003f176f3eaaed91bb2c
Matthew Gardiner <MatthewGardiner1@outlook.com>
2020-11-06 08:35:29 +1000

    Add project files.

---
+++ FitnessTracker/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using FitnessTracker.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(FitnessTracker.Areas.Identity.IdentityHostingStartup))]
+namespace FitnessTracker.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
